0d1808e206e232a1658335ed015eb2ec9d8b9f50
kimsj <sophia10047600@gmail.com>
2022-12-09 14:17:00 +0900

    파일 이동

--- project/src/App.js
+++ project/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 
-import Header from "./components/Header";
+import Header from "./components/Home/Header";
 import Body from "./components/Home/Body";
-import Footer from "./components/Footer";
+import Footer from "./components/Home/Footer";
 
 function App() {
   return (
