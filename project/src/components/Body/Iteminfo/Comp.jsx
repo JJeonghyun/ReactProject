ee8ca657a7d7858113a9bb774eba01f6a988be0e
YoungjunKim <color99b@naver.com>
2022-12-09 12:30:44 +0900

    Update Comp.jsx

--- project/src/components/Body/Iteminfo/Comp.jsx
+++ project/src/components/Body/Iteminfo/Comp.jsx
@@ -1,4 +1,5 @@
 // import Carousel from "react-material-ui-carousel";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 const imgArr = [
   {
@@ -12,6 +13,20 @@ const imgArr = [
   },
 ];
 const IteminfoComponent = () => {
+  const [current, setCurrent] = useState(0);
+  const [style, setStyle] = useState({ marginLeft: `-${current}00%` });
+  // const imgSize = document.getElementById("window").style.width;
+
+  const moveSlide = (i, imgSize = 1) => {
+    let nextIndex = current + i;
+    if (nextIndex < 0) nextIndex = imgSize.current - 1;
+    else if (nextIndex >= imgSize.length) nextIndex = 0;
+    console.log("들어옴");
+    setCurrent(nextIndex);
+  };
+  useEffect(() => {
+    setStyle({ marginLeft: `-${current}00%` });
+  }, [current]);
   return (
     <div>
       {/* <Carousel autoPlay={false}>
@@ -19,8 +34,33 @@ const IteminfoComponent = () => {
           <img key={index} src={item.imgAddress} style={{ width: "100%" }} />
         ))}
       </Carousel> */}
-
-      <Window></Window>
+      <Fbox>
+        <SlideBtn
+          onClick={() => {
+            moveSlide(-1);
+          }}
+        >
+          &lt;
+        </SlideBtn>
+        <Window id="window">
+          <Fbox style={style}>
+            {imgArr.map((item, index) => (
+              <img
+                key={index}
+                src={item.imgAddress}
+                style={{ width: "100%" }}
+              />
+            ))}
+          </Fbox>
+        </Window>
+        <SlideBtn
+          onClick={() => {
+            moveSlide(1);
+          }}
+        >
+          &gt;
+        </SlideBtn>
+      </Fbox>
     </div>
   );
 };
@@ -35,4 +75,15 @@ const Window = styled.div`
 
 const Fbox = styled.div`
   display: flex;
+  align-items: center;
+`;
+const SlideBtn = styled.button`
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  border-radius: 100%;
+  border: 1px solid black;
+  color: white;
+  background-color: black;
+  font-size: 2rem;
 `;
