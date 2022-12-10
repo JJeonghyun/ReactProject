13790cece964916f41a571dd4e79075c2c50e98b
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2022-12-10 17:08:09 +0900

    22.12.10 장바구니
    장바구니 드과자~

---
+++ project/src/pages/CartPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Home/Header";
+import CartePage from "../components/Home/Header/Cart/CartPage/index";
+import Footer from "../components/Home/Footer";
+const CartMainPage = () => {
+  return (
+    <>
+      <Header />
+      <CartePage />
+      <Footer />
+    </>
+  );
+};
+export default CartMainPage;
