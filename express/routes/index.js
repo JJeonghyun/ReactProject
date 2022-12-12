bdaa5ba9a33db2e539a6fbc41b3037ca4550eede
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-12-12 21:27:20 +0900

    221212 DB 기초 설정 연동 및 adminpage UI 구조 구현 완성

--- express/routes/index.js
+++ express/routes/index.js
@@ -2,4 +2,9 @@ import { Router } from "express";
 import product from "./products.js";
 const router = Router();
 router.use("/test", product);
+
+import upload from "./upload.js";
+
+router.use("/upload", upload);
+
 export default router;
