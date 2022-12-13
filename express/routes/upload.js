aa0d0f7d63fe9bc1cd49fa032be0ab02f41ed9c9
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-12-13 13:52:37 +0900

    221213 admin multer 라이브러리 적용

--- express/routes/upload.js
+++ express/routes/upload.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 const uploader = multer({ storage: storage });
 
 router.get("/upload", async (req, res) => {
-  fs.readdir("./upload", (err, data) => {
+  fs.readdir("/upload", (err, data) => {
     console.log("data : ", data);
     res.send({ data: data });
   });
@@ -26,7 +26,15 @@ router.post("/upload", uploader.single("product_img"), (req, res) => {
   console.log("파일 업로드");
   console.log(req.body);
 
-  res.send({ gdgd: "gdgd" });
+  res.send({
+    name: req.body.name,
+    productImg: req.file.filename,
+    model: req.body.model,
+    color: req.body.color,
+    account: req.body.account,
+    price: req.body.price,
+    info: req.body.info,
+  });
 });
 
 export default router;
