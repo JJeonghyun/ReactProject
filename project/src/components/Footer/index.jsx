255ce0b9cbf6118aedadb4217f7c1a138d208537
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-12-16 10:17:00 +0900

    userclone merge

---
+++ project/src/components/Footer/index.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <FooterBox>
+      <div>Tesla © 2022</div>
+      <div>개인정보 처리방침 및 법적고치</div>
+      <div>위치</div>
+    </FooterBox>
+  );
+};
+
+export default Footer;
+
+const FooterBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 50px 0;
+  color: black;
+`;
