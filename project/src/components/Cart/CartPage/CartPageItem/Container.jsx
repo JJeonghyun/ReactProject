a4354f11ee663aa6c027bcede6b8571aafee47a7
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2022-12-18 19:45:19 +0900

    22.12.18 스벅마지막타임 useEffect axios연결

--- project/src/components/Cart/CartPage/CartPageItem/Container.jsx
+++ project/src/components/Cart/CartPage/CartPageItem/Container.jsx
@@ -1,8 +1,21 @@
 import CartPageItem from "./Comp";
 
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
 const CartPageItemContainer = () => {
   const cartList = useSelector((state) => state.cartDB);
+  useEffect(() => {
+    try {
+      async function addCartList() {
+        const response = await axios.get("http://localhost:3000/cart");
+        console.log(response);
+      }
+      addCartList();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return <>{<CartPageItem cartList={cartList} />}</>;
 };
 
