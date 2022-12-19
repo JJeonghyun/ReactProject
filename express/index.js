c616f9ce4b9599e3cf1f1fb6cdf3d0cbd23d9c8f
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-12-19 21:37:09 +0900

    221219 last commit

--- express/index.js
+++ express/index.js
@@ -22,6 +22,7 @@ app.use((req, res, next) => {
   else morgan("dev")(req, res, next);
 });
 
+app.use("/upload", express.static("upload"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
