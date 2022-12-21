3c70f60a112620c8e2c91f217436812358efa6d0
JJeonghyun <111038274+JJeonghyun@users.noreply.github.com>
2022-12-21 16:16:25 +0900

    221221 1차 로그인 및 관리자

--- express/routes/user.js
+++ express/routes/user.js
@@ -5,7 +5,7 @@ import Cryptojs from "crypto-js";
 import db from "../models/index.js";
 
 const router = Router();
-// const userArr = [];
+const userArr = [];
 // const user = {};
 
 // router.post("/regist", async (req, res) => {
@@ -14,24 +14,42 @@ const router = Router();
 //   res.end({ message: "db 들어간다" });
 // });
 
-router.post("/regist", (req, res) => {
-  console.log("client 받음", req.body);
-  db.User.create({
-    userEmail: req.body.userEmail,
-    userPw: Cryptojs.SHA256(req.body.userPw).toString(),
-    userLastName: req.body.userLastName[0],
-    userFirstName: req.body.userFirstName[0],
-  }).then((data) => {
-    res.send({ message: "db에 잘 저장됨" });
-  });
+router.post("/regist", async (req, res) => {
+  try {
+    console.log("client 받음", req.body);
+    const [userEmail, userPw, userFirstName, userLastName] = req.body;
+    console.log("asd", userEmail, userPw, userFirstName, userLastName);
+    const tempEmail = await db.User.findOne({
+      where: { userEmail: req.body.userEmail },
+    });
+
+    if (tempEmail) {
+      res.send({ message: "ID 줭복" });
+      return;
+    } else {
+      db.User.create({
+        userEmail: req.body.userEmail,
+        userPw: Cryptojs.SHA256(req.body.userPw).toString(),
+        userLastName: req.body.userLastName[0],
+        userFirstName: req.body.userFirstName[0],
+      }).then((data) => {
+        res.send({ message: "db에 잘 저장됨" });
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/login", async (req, res) => {
   // console.log(req.body.userList);
   // console.log(userArr);
-  const tempcheck = userArr.find(
-    (item) => item.userEmail === req.body.userEmail
-  );
+
+  const tempcheck = db.User.findOne({
+    where: {
+      userEmail: req.body.userEmail,
+    },
+  });
   try {
     if (!tempcheck) {
       res.send({ msg: "ID가 없어요" });
@@ -80,6 +98,11 @@ router.post("/login", async (req, res) => {
 router.get("/logout", (req, res) => {
   res.clearCookie("jjh");
   res.end();
+});
+
+router.get("/list", async (req, res) => {
+  const list = await db.User.findAll();
+  res.send({ list: list });
 });
 
 export default router;
