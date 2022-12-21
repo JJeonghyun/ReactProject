dc72d114829f28b9ae03271f4dcca3df4f7f53fa
YoungjunKim <color99b@naver.com>
2022-12-21 17:46:34 +0900

    DB수정전 push

--- project/src/components/Cart/CartPage/CartPageItem/Container.jsx
+++ project/src/components/Cart/CartPage/CartPageItem/Container.jsx
@@ -1,22 +1,25 @@
 import CartPageItem from "./Comp";
 
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-const CartPageItemContainer = () => {
+const CartPageItemContainer = ({ setTotalState }) => {
   const cartList = useSelector((state) => state.cartDB);
+  const [list, setList] = useState([]);
   useEffect(() => {
     try {
       async function addCartList() {
-        const response = await axios.get("http://localhost:8080/cart");
-        console.log(response);
+        const tempList = await axios.post("http://localhost:8080/api/cart/");
+        setList(tempList.data.list);
       }
       addCartList();
     } catch (error) {
       console.log(error);
     }
   }, []);
-  return <>{<CartPageItem cartList={cartList} />}</>;
+
+  // return <>{<CartPageItem cartList={cartList} />}</>;
+  return <>{<CartPageItem cartList={list} setTotalState={setTotalState} />}</>;
 };
 
 export default CartPageItemContainer;
